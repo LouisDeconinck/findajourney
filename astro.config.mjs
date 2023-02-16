7b249181039d8342b02eeb3f18e1a88f05467c65
Louis Deconinck <louis.dck@gmail.com>
2023-02-17 00:03:34 +0100

    changed github deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://louisdeconinck.github.io',
+  base: '/findajourney',
   integrations: [tailwind()]
 });
