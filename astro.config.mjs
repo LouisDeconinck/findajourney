cd8d205e816825647b4f87daad943b8953ed0ae9
Louis Deconinck <louis.dck@gmail.com>
2023-02-17 08:47:19 +0100

    change site to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://louisdeconinck.github.io',
+  site: 'https://stellar-fox-31c85d.netlify.app/',
   base: '/findajourney',
   integrations: [tailwind()]
 });
